852cf06ca9fff0aa6351e5494062ec07640d6def
gabrielteodoroo <biel.teodorob@gmail.com>
2023-08-28 16:38:03 -0300

    create expert endpoint

---
+++ src/experts/experts.controller.ts
@@ -0,0 +1,31 @@
+import {
+	BadRequestException,
+	Body,
+	Controller,
+	Get,
+	HttpStatus,
+	Post,
+	Res
+} from '@nestjs/common'
+import { ExpertsService } from './experts.service'
+import CreateExpertsDto from './dtos/create-experts'
+import { Response } from 'express'
+
+@Controller('experts')
+export class ExpertsController {
+	constructor(private readonly expertsService: ExpertsService) {}
+
+	@Post()
+	async create(@Body() data: CreateExpertsDto, @Res() res: Response) {
+		const expertExists = await this.expertsService.findExpertByEmail(data.email)
+
+		if (expertExists) {
+			throw new BadRequestException(
+				'Já existe um profissional com o email informado'
+			)
+		}
+
+		const expert = await this.expertsService.createExpert(data)
+		return res.status(HttpStatus.CREATED).json(expert)
+	}
+}
