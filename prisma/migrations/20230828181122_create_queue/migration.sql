28f9e4aaebbbeb2cb69b712e2faaa5b4b88bf45c
gabrielteodoroo <biel.teodorob@gmail.com>
2023-08-28 15:18:03 -0300

    create model and migration of queue with relation

---
+++ prisma/migrations/20230828181122_create_queue/migration.sql
@@ -0,0 +1,14 @@
+-- AlterTable
+ALTER TABLE "experts" ADD COLUMN     "phone" TEXT;
+
+-- CreateTable
+CREATE TABLE "queues" (
+    "id" TEXT NOT NULL,
+    "createdAt" DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "expertId" TEXT NOT NULL,
+
+    CONSTRAINT "queues_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "queues" ADD CONSTRAINT "queues_expertId_fkey" FOREIGN KEY ("expertId") REFERENCES "experts"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
