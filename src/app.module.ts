fda8f52729bff32a8ba973f32dd7e3179dbdaa96
gabrielteodoroo <biel.teodorob@gmail.com>
2023-08-28 20:12:22 -0300

    implemented add customer queue

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
 import { PrismaModule } from './database/prisma.module'
 import { ExpertsModule } from './experts/experts.module'
-import { QueuesModule } from './queues/queues.module';
+import { QueuesModule } from './queues/queues.module'
+import { QueuecustomersModule } from './queuecustomers/queuecustomers.module'
 
 @Module({
-	imports: [PrismaModule, ExpertsModule, QueuesModule],
+	imports: [PrismaModule, ExpertsModule, QueuesModule, QueuecustomersModule],
 	controllers: [],
 	providers: []
 })
